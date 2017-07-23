fd0c6c29af342ee6ca58af202e8b899ae327f081
anubhav-trivedi <anubhav.trivedi09@gmail.com>
2017-07-23 15:50:40 +0530

    Added new Employee class

---
+++ CsharpConcepts/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpConcepts
+{
+    public class Employee
+    {
+
+    }
+
+    public class FullTimeEmployee : Employee
+    {   
+ 
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        
+    }
+
+    public class TemporaryEmployee : Employee
+    {
+        
+    }
+
+}
